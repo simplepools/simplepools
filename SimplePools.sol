924af19b79f858a75a3ab87c215b69debff943bc
Simple Pools <simplepools@proton.me>
2023-01-22 20:15:07 +0200

    Add files via upload

--- SimplePools.sol
+++ SimplePools.sol
@@ -1,13 +1,18 @@
 pragma solidity ^0.8.17;
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Simple Pools smart contract DeFi exchange.
-// Copyright (C) 2023 simplepools.org
+// Copyright (C) 2023 Simple Pools
 
-// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software 
+// Foundation, either version 3 of the License, or (at your option) any later version.
 
-// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+// This program is distributed in the hope that it will be useful, but WITHOUT 
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+// FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
-// You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU General Public License along with this 
+// program. If not, see <https://www.gnu.org/licenses/>.
 
 /**
  * Simple Pools
