9317f78013fbf9d3c3024e25e659fe44d4a9e648
simplepools <contact@simplepools.org>
2023-02-24 12:06:34 +0200

    Modify workflow.

--- startup.sh
+++ startup.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 pkill java
 rm /home/webapp-0.0.1-SNAPSHOT.jar
-cp /home/tmp/simplepools-webapp/target/webapp-0.0.1-SNAPSHOT.jar /home/webapp-0.0.1-SNAPSHOT.jar
-java -jar /home/webapp-0.0.1-SNAPSHOT.jar org.simplepools.webapp.WebappApplication > /home/logs.txt 2> /home/error.txt &
-echo "Server successfully restarted."
+java -jar /home/webapp/simplepools-webapp/target/webapp-0.0.1-SNAPSHOT.jar org.simplepools.webapp.WebappApplication > /home/webapp/logs/logs.txt 2> /home/webapp/logs/error.txt &
+echo "Server successfully restarted. Version 2."
