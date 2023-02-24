b00bd000f7fb8e975c43d3fd1c3012603a0af201
simplepools <contact@simplepools.org>
2023-02-24 12:12:55 +0200

    Test

--- startup.sh
+++ startup.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 pkill java
-rm /home/webapp-0.0.1-SNAPSHOT.jar
 java -jar /home/webapp/simplepools-webapp/target/webapp-0.0.1-SNAPSHOT.jar org.simplepools.webapp.WebappApplication > /home/webapp/logs/logs.txt 2> /home/webapp/logs/error.txt &
-echo "Server successfully restarted. Version 2."
+echo "Server successfully restarted. Version 3."
